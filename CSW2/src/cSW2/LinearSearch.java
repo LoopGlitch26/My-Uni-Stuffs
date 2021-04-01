cd993e5492327e64cc817c3d48ac6f2b94f23dc6
Bravish Ghosh <grandson.baba.2012@gmail.com>
2021-04-01 07:34:24 +0530

    LinearSearch updated

--- CSW2/src/cSW2/LinearSearch.java
+++ CSW2/src/cSW2/LinearSearch.java
@@ -1,5 +1,51 @@
 package cSW2;
 
+import java.util.Scanner;
+
 public class LinearSearch {
+	
+	public int UnsortedLinearSearch(int[] A, int data) {
+		for (int i = 0; i < A.length; i++) { // Time complexity of worst case is O(n) because in worst case we need to scan the complete array
+			if (A[i] == data)
+				return i;
+		}
+		return -1;
+	}
+	
+/*	public int SortedLinearSearch(int[] A, int data) {
+		for (int i = 0; i < A.length; i++) { // Time complexity of worst case is O(n) because in worst case we need to scan the complete array
+			if (A[i] == data)
+				return i;
+			else 
+				if(A[i] > data) // Sorted data position check
+					return -1;			
+		}
+		return -1;
+	}*/
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.println("Enter the size of the array");
+			int s = sc.nextInt();
+			
+			System.out.println("Enter the array elements");
+			int[] A = new int[s];
+			for(int i=0; i<s; i++)
+				A[i] = sc.nextInt();
+			
+			System.out.println("Enter the element to be searched");
+			int data = sc.nextInt();
+			
+			LinearSearch ob = new LinearSearch();			
+			System.out.println(ob.UnsortedLinearSearch(A, data));
+//			System.out.println(ob.SortedLinearSearch(A, data));
+		}
+		finally {
+			sc.close();
+		}
+		
+	}
 
 }
